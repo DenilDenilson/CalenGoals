f57f92e71dfddeb74f6e1471ac81be9efed191eb
DenilDenilson <jordanpreseleccion@gmail.com>
2023-03-18 10:08:19 -0500

    Añadiendo user_loader

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 from flask import Flask, render_template, request, redirect, url_for
-# from flask_login import LoginManager
+from flask_login import LoginManager
 from models import user
 
 app =Flask(__name__)
 app.config["SECRET_KEY"] = "so-secret"
 
-# login_manager = LoginManager()
-# login_manager.init_app(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 @app.route("/")
 def hello_world():
-    return render_template('index.html')
+    return render_template('signup.html')
 
 @app.route("/signup", methods=['POST'])
 def signup():
@@ -27,3 +27,11 @@ def login():
 @app.route("/home")
 def home():
     return "B I E N V E N I D O"
+
+# C A R G A R   U S U A R I O S
+@login_manager.user_loader
+def load_user(alias):
+    for user in user.db_user:
+        if user.alias == alias:
+            return user
+        return None
