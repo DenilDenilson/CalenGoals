e03462d3cad6b7cf491b13e3a0ad3cc8e7969c1c
DenilDenilson <jordanpreseleccion@gmail.com>
2023-03-18 12:03:54 -0500

    Logica por ahí, seguir lo que pide la biblioteca y mejorar la validación

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
-from flask_login import LoginManager
+from flask_login import LoginManager, login_user
 from models import user
 
 app =Flask(__name__)
@@ -8,25 +8,45 @@ app.config["SECRET_KEY"] = "so-secret"
 login_manager = LoginManager()
 login_manager.init_app(app)
 
+db_user = user.db_user
+
 @app.route("/")
-def hello_world():
-    return render_template('signup.html')
+def home():
+    return "B I E N V E N I D O"
+
+@app.route("/sign-up")
+def sign_up():
+    return render_template('signup.html', users = user.db_user)
+
+@app.route("/log-in")
+def log_in():
+    return render_template("login.html", users=user.db_user)
 
 @app.route("/signup", methods=['POST'])
 def signup():
-    new_user = user.User(request.form['alias'], request.form['password'])
+    alias = request.form['alias']
+    password = request.form['password']
+    new_user = user.User(alias, password)
     user.db_user.append(new_user)
     print(user.db_user)
-    return redirect(url_for("hello_world"))
+    return redirect(url_for("sign_up"))
     
 @app.route("/login", methods=['POST'])
-def login():
-    return "Logeado"
+def login():    
+    print(db_user)
+    valid_alias = False
+    valid_password = True
+    for user in db_user:
+        if user.alias == request.form['alias']:
+            valid_alias = True
+        if user.password == request.form['password']:
+            valid_password = True
+        if valid_alias and valid_password:
+          login_user(user)
+          return redirect(url_for("home"))
+    return redirect(url_for("log_in"))
 
 
-@app.route("/home")
-def home():
-    return "B I E N V E N I D O"
 
 # C A R G A R   U S U A R I O S
 @login_manager.user_loader
